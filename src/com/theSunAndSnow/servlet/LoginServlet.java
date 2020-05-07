4ad3ab5e234e2fdb7568f6dca2af5f95086c0e10
theSunAndSnow <55275586+theSunAndSnow@users.noreply.github.com>
2020-05-07 23:33:41 +0800

    第一次提交

---
+++ src/com/theSunAndSnow/servlet/LoginServlet.java
@@ -0,0 +1,25 @@
+package com.theSunAndSnow.servlet;
+
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
+    /**
+     * 处理登陆的逻辑
+     * @param req
+     * @param resp
+     * @throws ServletException
+     * @throws IOException
+     */
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String username = req.getParameter("username"),
+                password = req.getParameter("password");
+    }
+}
